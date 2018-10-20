e6640a3de913ecfbfd0a878ea02db6b06f5a738c
tom <tom@service.com>
2018-10-20 16:43:09 +0800

    ok

--- springboot-dubbo-consumer/src/main/java/com/niuzj/consumer/SpringbootDubboConsumerApplication.java
+++ springboot-dubbo-consumer/src/main/java/com/niuzj/consumer/SpringbootDubboConsumerApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-@ImportResource("classpath:dubbo-consumer.xml")
+@ImportResource("classpath:dubbo-test.xml")
 public class SpringbootDubboConsumerApplication {
 
     public static void main(String[] args) {
--- springboot_dubbo_provider/src/main/java/com/niuzj/provider/ProviderApplication.java
+++ springboot_dubbo_provider/src/main/java/com/niuzj/provider/ProviderApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-@ImportResource("classpath:dubbo-provider.xml")
+@ImportResource("classpath:muti-protocol.xml")
 public class ProviderApplication {
 
     public static void main(String[] args) {
